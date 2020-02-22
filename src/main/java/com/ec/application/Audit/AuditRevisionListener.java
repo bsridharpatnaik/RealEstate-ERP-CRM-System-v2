05ea87a7f91fe59816b1bdc7321078782c6d6928
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2020-02-23 01:47:21 +0530

    Username While Revision

--- src/main/java/com/ec/application/Audit/AuditRevisionListener.java
+++ src/main/java/com/ec/application/Audit/AuditRevisionListener.java
@@ -3,12 +3,15 @@ package com.ec.application.Audit;
 
 
 import org.hibernate.envers.RevisionListener;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 public class AuditRevisionListener implements RevisionListener {
 
     @Override
     public void newRevision(Object revisionEntity) {
         AuditRevisionEntity audit = (AuditRevisionEntity) revisionEntity;
-        audit.setUsername("admin");
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        audit.setUsername(auth.getName());
     }
 }
