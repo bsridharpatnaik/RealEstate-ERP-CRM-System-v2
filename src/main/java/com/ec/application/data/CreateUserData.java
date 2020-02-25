c8a3859d4d8094d52f7bbe40e792b4f8ba3ddacb
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2020-02-25 10:02:24 +0530

    UserChanges

---
+++ src/main/java/com/ec/application/data/CreateUserData.java
@@ -0,0 +1,26 @@
+package com.ec.application.data;
+
+public class CreateUserData 
+{
+	String username;
+	String password;
+	String role;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
--- src/main/java/com/ec/application/service/UserService.java
+++ src/main/java/com/ec/application/service/UserService.java
@@ -5,11 +5,12 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
+import com.ec.application.data.CreateUserData;
 import com.ec.application.model.Role;
 import com.ec.application.model.User;
 import com.ec.application.repository.RoleRepo;
@@ -24,20 +25,27 @@ public class UserService
 	@Autowired
 	RoleRepo rRepo;
 	
-	public User createUser(String username, String password, String role) throws Exception 
-	{
-		User user = new User();
-		
-		Role roleEntity = rRepo.findByName(role);
-		Set<Role> roleset = new HashSet<Role>();
-		roleset.add(roleEntity);
-		user.setUserName(username);
-		user.setStatus(true);
-		user.setRoles(roleset);
-		user.setPassword(bCryptPassword(password));
-		user.setPasswordExpired(false);
-		uRepo.save(user);
-		return user;
+	public User createUser(CreateUserData userData)
+	{		
+		String username = userData.getUsername();
+		String password = userData.getPassword();
+		String role = userData.getRole(); 
+	
+		if(!rRepo.findByUserName(username))
+		{
+			User user = new User();
+			
+			Role roleEntity = rRepo.findByName(role);
+			Set<Role> roleset = new HashSet<Role>();
+			roleset.add(roleEntity);
+			user.setUserName(username);
+			user.setStatus(true);
+			user.setRoles(roleset);
+			user.setPassword(bCryptPassword(password));
+			user.setPasswordExpired(false);
+			uRepo.save(user);
+			return user;
+		}
 	}
 
 	public String bCryptPassword(String password)
@@ -47,6 +55,11 @@ public class UserService
 		return bcyptedPassword;
 	}
 
+	public Page<User> fetchAll(Pageable pageable)
+	{
+		return uRepo.findAll(pageable);
+	}
+	
 	public void updateUser(String username, String password) throws Exception 
 	{
 		ArrayList<User> users = uRepo.findUserByUsername(username);
