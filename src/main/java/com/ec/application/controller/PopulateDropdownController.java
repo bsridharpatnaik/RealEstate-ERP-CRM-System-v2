43d64b2061fdf655304182c232dcb70b65737059
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2020-03-14 17:37:31 +0530

    dropdown controller

--- src/main/java/com/ec/application/controller/PopulateDropdownController.java
+++ src/main/java/com/ec/application/controller/PopulateDropdownController.java
@@ -1,17 +1,12 @@
 package com.ec.application.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ec.application.data.MORDropdownData;
 import com.ec.application.data.NameAndProjectionDataForDropDown;
-import com.ec.application.model.BasicEntities.Product;
 import com.ec.application.service.PopulateDropdownService;
 
 @RestController
@@ -22,9 +17,9 @@ public class PopulateDropdownController
 	@Autowired
 	PopulateDropdownService pdService;
 	
-	@GetMapping("/mor")
-	public NameAndProjectionDataForDropDown findProductbyvehicleNoProducts() 
+	@GetMapping("/{page}")
+	public NameAndProjectionDataForDropDown findDropdownValuesForMOR(@PathVariable String page) 
 	{
-		return pdService.fetchData();
+		return pdService.fetchData(page);
 	}
 }
