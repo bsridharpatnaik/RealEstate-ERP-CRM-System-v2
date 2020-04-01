5874d8d9347472429e3286be8f9f36d22cacee0b
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2020-04-01 18:51:58 +0530

    Update EcApplication.java

--- src/main/java/com/ec/application/EcApplication.java
+++ src/main/java/com/ec/application/EcApplication.java
@@ -3,10 +3,10 @@ package com.ec.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class EcApplication  extends SpringBootServletInitializer{
 	private static final String ZONE_ID_INDIA = "Asia/Kolkata";
 			
