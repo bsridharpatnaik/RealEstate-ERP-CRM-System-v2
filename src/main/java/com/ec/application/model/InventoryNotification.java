1ddbb56e84a985a875c83d3d2e4d193ae3c4658f
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2020-05-12 14:11:57 +0530

    Update InventoryNotification.java

---
+++ src/main/java/com/ec/application/model/InventoryNotification.java
@@ -0,0 +1,61 @@
+package com.ec.application.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Where;
+import org.hibernate.envers.Audited;
+
+import com.ec.application.ReusableClasses.ReusableFields;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "inventorynotification")
+@Audited
+@Where(clause = ReusableFields.SOFT_DELETED_CLAUSE)
+public class InventoryNotification extends ReusableFields
+{
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="id")
+	Long notificationId;
+	
+	@ManyToOne(fetch=FetchType.LAZY,cascade = CascadeType.ALL)
+	@JoinColumn(name="productId",nullable=false)
+	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+	Product product;
+	
+	String type;
+
+	public Long getNotificationId() {
+		return notificationId;
+	}
+
+	public void setNotificationId(Long notificationId) {
+		this.notificationId = notificationId;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+}
