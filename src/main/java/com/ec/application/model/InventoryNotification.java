5c27de30671d703c5183275663c5680ec0167fac
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2020-05-12 17:12:55 +0530

    Update InventoryNotification.java

---
+++ src/main/java/com/ec/application/model/InventoryNotification.java
@@ -0,0 +1,49 @@
+package com.ec.application.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Where;
+import org.hibernate.envers.Audited;
+
+import com.ec.application.ReusableClasses.ReusableFields;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "inventorynotification")
+@Audited
+@Where(clause = ReusableFields.SOFT_DELETED_CLAUSE)
+public class InventoryNotification extends ReusableFields
+{
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	Long Id;
+	
+	@ManyToOne(fetch=FetchType.LAZY,cascade = CascadeType.ALL)
+	@JoinColumn(name="productId",nullable=false)
+	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+	Product product;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+}
