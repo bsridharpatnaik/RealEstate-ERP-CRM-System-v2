24a10f8781b38f4c75f0f00936c29cd7a8f22896
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2020-05-13 08:22:15 +0530

    Notification Working

--- src/main/java/com/ec/application/service/AllNotificationService.java
+++ src/main/java/com/ec/application/service/AllNotificationService.java
@@ -27,6 +27,7 @@ public class AllNotificationService
 
 	private List<ReturnSingleNotification> getInventoryNotification() 
 	{
+		String source = "inventory";
 		List<ReturnSingleNotification> inventoryNormalizedNotifications = new ArrayList<ReturnSingleNotification> ();
 		List<InventoryNotification> inventoryNotifications = inventoryNotificationService.returnInventoryNotifications();
 		
@@ -38,7 +39,8 @@ public class AllNotificationService
 			{
 				message= "Product " + inventoryNotification.getProduct().getProductName() +" is low on stock";
 			}
-			ReturnSingleNotification returnAllNotification = new ReturnSingleNotification(inventoryNotification.getId(),"lowStock",message);
+			ReturnSingleNotification returnAllNotification = new ReturnSingleNotification
+					(inventoryNotification.getId(),source,"lowStock",message,inventoryNotification.getCreated(),inventoryNotification.getModified());
 			inventoryNormalizedNotifications.add(returnAllNotification);
 		}
 		return inventoryNormalizedNotifications;
