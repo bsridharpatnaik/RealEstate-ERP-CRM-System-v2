3f206576926ed4c72fdcded75d113a9a5b266de6
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2020-07-21 01:02:36 +0530

    Config for tomcat support

--- ec-crm-service/src/main/java/com/ec/crm/CrmApplication.java
+++ ec-crm-service/src/main/java/com/ec/crm/CrmApplication.java
@@ -2,9 +2,10 @@ package com.ec.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CrmApplication {
+public class CrmApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrmApplication.class, args);
