2687692bd17d1fc03dcf9a2e857a6db1bfcf9d37
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2020-08-23 23:28:43 +0530

    Mapstruct with lombok

--- ec-crm-service/src/main/java/com/ec/crm/Data/LeadActivityDTO.java
+++ ec-crm-service/src/main/java/com/ec/crm/Data/LeadActivityDTO.java
@@ -24,9 +24,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import Deserializers.ToUsernameSerializer;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-//@Data
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LeadActivityDTO 
 {
 	
@@ -58,115 +62,4 @@ public class LeadActivityDTO
 	
 	@Enumerated(EnumType.STRING)
 	ActivityTypeEnum activityType;
-
-	public LeadActivityDTO() {
-		super();
-	}
-
-	public LeadActivityDTO(Long leadActivityId, Date activityDateTime, String title, String description,
-			List<String> tags, Boolean isOpen, Long creatorId, Long closedBy, Lead lead, String closingComment,
-			ActivityTypeEnum activityType) {
-		super();
-		this.leadActivityId = leadActivityId;
-		this.activityDateTime = activityDateTime;
-		this.title = title;
-		this.description = description;
-		this.tags = tags;
-		this.isOpen = isOpen;
-		this.creatorId = creatorId;
-		this.closedBy = closedBy;
-		this.lead = lead;
-		this.closingComment = closingComment;
-		this.activityType = activityType;
-	}
-
-	public Long getLeadActivityId() {
-		return leadActivityId;
-	}
-
-	public void setLeadActivityId(Long leadActivityId) {
-		this.leadActivityId = leadActivityId;
-	}
-
-	public Date getActivityDateTime() {
-		return activityDateTime;
-	}
-
-	public void setActivityDateTime(Date activityDateTime) {
-		this.activityDateTime = activityDateTime;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public List<String> getTags() {
-		return tags;
-	}
-
-	public void setTags(List<String> tags) {
-		this.tags = tags;
-	}
-
-	public Boolean getIsOpen() {
-		return isOpen;
-	}
-
-	public void setIsOpen(Boolean isOpen) {
-		this.isOpen = isOpen;
-	}
-
-	public Long getCreatorId() {
-		return creatorId;
-	}
-
-	public void setCreatorId(Long creatorId) {
-		this.creatorId = creatorId;
-	}
-
-	public Long getClosedBy() {
-		return closedBy;
-	}
-
-	public void setClosedBy(Long closedBy) {
-		this.closedBy = closedBy;
-	}
-
-	public Lead getLead() {
-		return lead;
-	}
-
-	public void setLead(Lead lead) {
-		this.lead = lead;
-	}
-
-	public String getClosingComment() {
-		return closingComment;
-	}
-
-	public void setClosingComment(String closingComment) {
-		this.closingComment = closingComment;
-	}
-
-	public ActivityTypeEnum getActivityType() {
-		return activityType;
-	}
-
-	public void setActivityType(ActivityTypeEnum activityType) {
-		this.activityType = activityType;
-	}
-	
-	
 }
