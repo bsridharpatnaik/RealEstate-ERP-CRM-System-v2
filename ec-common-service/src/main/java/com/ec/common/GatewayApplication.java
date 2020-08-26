ace50377e4612d0b99346216b26deb21904463e9
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2020-08-27 01:02:08 +0530

    Request Log

--- ec-common-service/src/main/java/com/ec/common/GatewayApplication.java
+++ ec-common-service/src/main/java/com/ec/common/GatewayApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 //import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @EnableZuulProxy
 //@EnableEurekaServer
@@ -23,4 +25,14 @@ public class GatewayApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+	    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+	    loggingFilter.setIncludeClientInfo(true);
+	    loggingFilter.setIncludeQueryString(true);
+	    loggingFilter.setIncludePayload(true);
+	    loggingFilter.setMaxPayloadLength(64000);
+	    return loggingFilter;
+	}
+
 }
