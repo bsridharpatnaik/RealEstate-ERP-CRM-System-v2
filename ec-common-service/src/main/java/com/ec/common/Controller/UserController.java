1747b09ca71c99e1dfe4a0efe51d1553a224b989
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2020-09-22 00:59:47 +0530

    id from username

--- ec-common-service/src/main/java/com/ec/common/Controller/UserController.java
+++ ec-common-service/src/main/java/com/ec/common/Controller/UserController.java
@@ -26,82 +26,92 @@ import com.ec.common.Filters.FilterDataList;
 import com.ec.common.Model.User;
 import com.ec.common.Service.UserService;
 
-
 @RestController
-@RequestMapping(value="/user",produces = { "application/json", "text/json" })
-public class UserController 
+@RequestMapping(value = "/user", produces =
+{ "application/json", "text/json" })
+public class UserController
 {
 
 	@Autowired
 	UserService userService;
-	
+
 	@GetMapping
 	@PreAuthorize("hasAuthority('admin')")
-	public UserListWithTypeAheadData returnAllUsers(Pageable pageable) 
+	public UserListWithTypeAheadData returnAllUsers(Pageable pageable)
 	{
-		
+
 		return userService.fetchAll(pageable);
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.OK)
-	public UserListWithTypeAheadData returnFilteredContacts(@RequestBody FilterDataList contactFilterDataList,@PageableDefault(page = 0, size = 10, sort = "userId", direction = Direction.DESC) Pageable pageable) 
+	public UserListWithTypeAheadData returnFilteredContacts(@RequestBody FilterDataList contactFilterDataList,
+			@PageableDefault(page = 0, size = 10, sort = "userId", direction = Direction.DESC) Pageable pageable)
 	{
-		return userService.findFilteredUsers(contactFilterDataList,pageable);
+		return userService.findFilteredUsers(contactFilterDataList, pageable);
 	}
-	
-	@PostMapping("/create") 
+
+	@PostMapping("/create")
 	@ResponseStatus(HttpStatus.CREATED)
 	@PreAuthorize("hasAuthority('admin')")
-	public User createUser(@RequestBody CreateUserData payload) throws Exception{
-		
+	public User createUser(@RequestBody CreateUserData payload) throws Exception
+	{
+
 		return userService.createUser(payload);
 	}
-	
-	@PostMapping("/updatepassword") 
+
+	@PostMapping("/updatepassword")
 	@PreAuthorize("hasAuthority('admin')")
 	@ResponseStatus(HttpStatus.OK)
-	public User updateUser(@RequestBody ResetPasswordData payload) throws Exception{
-		
+	public User updateUser(@RequestBody ResetPasswordData payload) throws Exception
+	{
+
 		return userService.resetPassword(payload);
 	}
-	
+
 	@GetMapping("/{id}")
-	public User findUserByID(@PathVariable long id) throws Exception 
+	public User findUserByID(@PathVariable long id) throws Exception
 	{
 		return userService.findSingleUserFromAll(id);
 	}
-	
+
 	@GetMapping("/byid/{id}")
-	public UserReturnData findLimitedDetailsForUserByID(@PathVariable long id) throws Exception 
+	public UserReturnData findLimitedDetailsForUserByID(@PathVariable long id) throws Exception
 	{
 		return userService.fetchUserDetailsById(id);
 	}
-	
+
+	@GetMapping("/byname/{name}")
+	public UserReturnData findLimitedDetailsForUserByID(@PathVariable String name) throws Exception
+	{
+		return userService.fetchUserDetailsByName(name);
+	}
+
 	@GetMapping("/list")
-	public List<UserReturnData> findLimitedDetailsForUserByID() throws Exception 
+	public List<UserReturnData> findLimitedDetailsForUserByID() throws Exception
 	{
 		return userService.fetchUserList();
 	}
-	
-	@PostMapping("/updateroles") 
+
+	@PostMapping("/updateroles")
 	@PreAuthorize("hasAuthority('admin')")
 	@ResponseStatus(HttpStatus.OK)
-	public User updateRolesForUser(@RequestBody UpdateRolesForUserData payload) throws Exception{
-		
+	public User updateRolesForUser(@RequestBody UpdateRolesForUserData payload) throws Exception
+	{
+
 		return userService.updateRolesForUser(payload);
 	}
-	
-	@GetMapping(value="/me",produces="application/json")
-	public UserReturnData returnUserName() 
+
+	@GetMapping(value = "/me", produces = "application/json")
+	public UserReturnData returnUserName()
 	{
 		return userService.fetchUserDetails();
 	}
-	
+
 	@PutMapping("/{id}")
 	@PreAuthorize("hasAuthority('admin')")
-	public User updateUser(@PathVariable Long id, @RequestBody CreateUserData payload) throws Exception 
+	public User updateUser(@PathVariable Long id, @RequestBody CreateUserData payload) throws Exception
 	{
 		return userService.updateUser(id, payload);
-	} 
+	}
 }
--- ec-common-service/src/main/java/com/ec/common/Service/UserService.java
+++ ec-common-service/src/main/java/com/ec/common/Service/UserService.java
@@ -34,29 +34,33 @@ import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
-public class UserService {
+public class UserService
+{
 	@Autowired
 	UserRepo uRepo;
 
 	@Autowired
 	RoleRepo rRepo;
 
-	//private static final Logger log = LoggerFactory.getLogger(UserService.class);
-	public User createUser(CreateUserData userData) throws Exception {
+	// private static final Logger log = LoggerFactory.getLogger(UserService.class);
+	public User createUser(CreateUserData userData) throws Exception
+	{
 		String username = userData.getUsername();
 		String password = userData.getPassword();
 		ArrayList<String> roles = userData.getRole();
 
-		if (uRepo.findUserByUsername(username).size() == 0) {
+		if (uRepo.findUserByUsername(username).size() == 0)
+		{
 			User user = new User();
 			Set<Role> roleset = new HashSet<Role>();
 
-			for (String role : roles) {
+			for (String role : roles)
+			{
 				Role roleEntity = rRepo.findByName(role);
-				if(roleEntity!=null)
+				if (roleEntity != null)
 					roleset.add(roleEntity);
 			}
-			if(roleset.size()<1)
+			if (roleset.size() < 1)
 				throw new Exception("Role(s) not Found!");
 			user.setUserName(username);
 			user.setStatus(true);
@@ -66,52 +70,58 @@ public class UserService {
 			uRepo.save(user);
 			return user;
 
-		} 
-		else 
+		} else
 		{
-			//log.info("User already exists");
+			// log.info("User already exists");
 			throw new Exception("User already exists");
 		}
 	}
 
-	public String bCryptPassword(String password) {
+	public String bCryptPassword(String password)
+	{
 		String bcyptedPassword;
 		bcyptedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
 		return bcyptedPassword;
 	}
 
-	public UserListWithTypeAheadData fetchAll(Pageable pageable) 
+	public UserListWithTypeAheadData fetchAll(Pageable pageable)
 	{
-		UserListWithTypeAheadData tpData  = new UserListWithTypeAheadData();
+		UserListWithTypeAheadData tpData = new UserListWithTypeAheadData();
 		tpData.setUserDetails(uRepo.findAll(pageable));
 		tpData.setRoles(rRepo.findRoleNames());
 		tpData.setUsernames(uRepo.findUserNames());
 		return tpData;
 	}
 
-	public User resetPassword(ResetPasswordData rpData) throws Exception {
+	public User resetPassword(ResetPasswordData rpData) throws Exception
+	{
 		String username = rpData.getUsername();
 		String password = rpData.getNewPassword();
 		ArrayList<User> users = uRepo.findUserByUsername(username);
-		if (users.size() == 1) {
+		if (users.size() == 1)
+		{
 			User user = users.get(0);
 			user.setPassword(bCryptPassword(password));
 			uRepo.save(user);
 			return user;
-		} else {
+		} else
+		{
 			throw new Exception("None or Multiple users found by username!");
 		}
 	}
 
-	public User updateRolesForUser(UpdateRolesForUserData upRoleData) throws Exception {
+	public User updateRolesForUser(UpdateRolesForUserData upRoleData) throws Exception
+	{
 		String username = upRoleData.getUsername();
 		ArrayList<String> roles = upRoleData.getRoles();
 		ArrayList<User> users = uRepo.findUserByUsername(username);
-		if (users.size() == 1) {
+		if (users.size() == 1)
+		{
 			User user = users.get(0);
 			Set<Role> roleset = new HashSet<Role>();
 
-			for (String role : roles) {
+			for (String role : roles)
+			{
 				Role roleEntity = rRepo.findByName(role);
 				if (roleEntity != null)
 					roleset.add(roleEntity);
@@ -119,70 +129,73 @@ public class UserService {
 			user.setRoles(roleset);
 			uRepo.save(user);
 			return user;
-		} else {
+		} else
+		{
 			throw new Exception("None or Multiple users found by username!");
 		}
 	}
 
-	public UserReturnData fetchUserDetails() 
+	public UserReturnData fetchUserDetails()
 	{
 		UserReturnData userReturnData = new UserReturnData();
 		List<String> roles = new ArrayList<String>();
 
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		userReturnData.setUsername(auth.getName());
-		Long id=uRepo.findId(auth.getName());
+		Long id = uRepo.findId(auth.getName());
 		userReturnData.setId(id);
 		Collection<SimpleGrantedAuthority> authorities = (Collection<SimpleGrantedAuthority>) SecurityContextHolder
 				.getContext().getAuthentication().getAuthorities();
-		for (SimpleGrantedAuthority authority : authorities) {
+		for (SimpleGrantedAuthority authority : authorities)
+		{
 			roles.add(authority.getAuthority());
 		}
 		userReturnData.setRoles(roles);
 		return userReturnData;
 	}
 
-	public UserReturnData fetchUserDetailsById(Long id) throws Exception 
+	public UserReturnData fetchUserDetailsById(Long id) throws Exception
 	{
 		UserReturnData userReturnData = new UserReturnData();
 		Optional<User> userOpt = uRepo.findById(id);
-		if(!userOpt.isPresent())
-			throw new Exception("User not found with ID -"+id);
-		
+		if (!userOpt.isPresent())
+			throw new Exception("User not found with ID -" + id);
+
 		User user = userOpt.get();
 		userReturnData.setUsername(user.getUserName());
 		userReturnData.setId(user.getUserId());
 		userReturnData.setRoles(fetchRolesFromSet(user.getRoles()));
 		return userReturnData;
 	}
-	
-	private List<String> fetchRolesFromSet(Set<Role> roleSet) 
-	{	
+
+	private List<String> fetchRolesFromSet(Set<Role> roleSet)
+	{
 		List<String> roles = new ArrayList<String>();
-		for(Role role:roleSet)
+		for (Role role : roleSet)
 		{
 			roles.add(role.getName());
 		}
 		return roles;
 	}
 
-	public List<UserReturnData> fetchUserList() 
-	{	
+	public List<UserReturnData> fetchUserList()
+	{
 		List<UserReturnData> userReturnDataList = new ArrayList<UserReturnData>();
 		List<User> userList = uRepo.findAll();
-		for(User user:userList)
+		for (User user : userList)
 		{
-			UserReturnData userReturnData = new UserReturnData(user.getUserId(),user.getUserName(),fetchRolesFromSet(user.getRoles()));
+			UserReturnData userReturnData = new UserReturnData(user.getUserId(), user.getUserName(),
+					fetchRolesFromSet(user.getRoles()));
 			userReturnDataList.add(userReturnData);
 		}
 		return userReturnDataList;
 	}
-	
-	public UserListWithTypeAheadData findFilteredUsers(FilterDataList contactFilterDataList, Pageable pageable) 
+
+	public UserListWithTypeAheadData findFilteredUsers(FilterDataList contactFilterDataList, Pageable pageable)
 	{
-		UserListWithTypeAheadData tpData  = new UserListWithTypeAheadData();
-		
-		tpData.setUserDetails(getFilteredData(contactFilterDataList,pageable));
+		UserListWithTypeAheadData tpData = new UserListWithTypeAheadData();
+
+		tpData.setUserDetails(getFilteredData(contactFilterDataList, pageable));
 		tpData.setRoles(rRepo.findRoleNames());
 		tpData.setUsernames(uRepo.findUserNames());
 		return tpData;
@@ -191,75 +204,88 @@ public class UserService {
 	public Page<User> getFilteredData(FilterDataList contactFilterDataList, Pageable pageable)
 	{
 		Specification<User> spec = fetchSpecification(contactFilterDataList);
-		if(spec!=null)
+		if (spec != null)
 			return uRepo.findAll(spec, pageable);
 		return uRepo.findAll(pageable);
 	}
-	
-	private Specification<User> fetchSpecification(FilterDataList contactFilterDataList) 
+
+	private Specification<User> fetchSpecification(FilterDataList contactFilterDataList)
 	{
 		Specification<User> specification = null;
-		for(FilterAttributeData attrData:contactFilterDataList.getFilterData())
+		for (FilterAttributeData attrData : contactFilterDataList.getFilterData())
 		{
 			String attrName = attrData.getAttrName();
 			List<String> attrValues = attrData.getAttrValue();
-			
+
 			Specification<User> internalSpecification = null;
-			for(String attrValue : attrValues)
+			for (String attrValue : attrValues)
 			{
-				internalSpecification= internalSpecification==null?
-						UserSpecifications.whereUsernameContains(attrValue)
-						:internalSpecification.or(UserSpecifications.whereUsernameContains(attrValue));
+				internalSpecification = internalSpecification == null
+						? UserSpecifications.whereUsernameContains(attrValue)
+						: internalSpecification.or(UserSpecifications.whereUsernameContains(attrValue));
 			}
-			specification= specification==null?internalSpecification:specification.and(internalSpecification);
+			specification = specification == null ? internalSpecification : specification.and(internalSpecification);
 		}
 		return specification;
 	}
 
-	public User findSingleUserFromAll(long id) throws Exception 
+	public User findSingleUserFromAll(long id) throws Exception
 	{
 		Optional<User> user = uRepo.findById(id);
-		if(user.isPresent())
+		if (user.isPresent())
 			return user.get();
 		else
 			throw new Exception("User ID not found");
 	}
 
-	public User updateUser(Long id, CreateUserData payload) throws Exception 
+	public User updateUser(Long id, CreateUserData payload) throws Exception
 	{
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-		
-		if(payload.getUsername().equalsIgnoreCase(auth.getName()))
+		if (payload.getUsername().equalsIgnoreCase(auth.getName()))
 			throw new Exception("Cannot update logged in user");
-		
-	    User user = findSingleUserFromAll(id);
-	    String username = payload.getUsername();
-	    if(!user.getUserName().equals(payload.getUsername()))
-	    {
-	    	if(uRepo.findUserNames().contains(username))
-	    		throw new Exception("Username already exists");
-	    }
-	    
-	    ArrayList < String > roles = payload.getRole();
-	    Set < Role > roleset = new HashSet < Role > ();
 
-	    for (String role: roles) {
-	        Role roleEntity = rRepo.findByName(role);
-	        if (roleEntity != null)
-	            roleset.add(roleEntity);
-	    }
-	    if (roleset.size() < 1)
-	        throw new Exception("Please select atleast one role!");
-	    user.setUserName(username);
-	    user.setStatus(true);
-	    user.setRoles(roleset);
-	    if(payload.getPassword()!= null && payload.getPassword()!= "")
-	    	user.setPassword(bCryptPassword(payload.getPassword()));
-	    user.setPasswordExpired(false);
-	    uRepo.save(user);
-	    return user;
+		User user = findSingleUserFromAll(id);
+		String username = payload.getUsername();
+		if (!user.getUserName().equals(payload.getUsername()))
+		{
+			if (uRepo.findUserNames().contains(username))
+				throw new Exception("Username already exists");
+		}
+
+		ArrayList<String> roles = payload.getRole();
+		Set<Role> roleset = new HashSet<Role>();
+
+		for (String role : roles)
+		{
+			Role roleEntity = rRepo.findByName(role);
+			if (roleEntity != null)
+				roleset.add(roleEntity);
+		}
+		if (roleset.size() < 1)
+			throw new Exception("Please select atleast one role!");
+		user.setUserName(username);
+		user.setStatus(true);
+		user.setRoles(roleset);
+		if (payload.getPassword() != null && payload.getPassword() != "")
+			user.setPassword(bCryptPassword(payload.getPassword()));
+		user.setPasswordExpired(false);
+		uRepo.save(user);
+		return user;
 	}
-	
-	
+
+	public UserReturnData fetchUserDetailsByName(String name) throws Exception
+	{
+		UserReturnData userReturnData = new UserReturnData();
+		ArrayList<User> userList = uRepo.findUserByUsername(name);
+		if (userList.size() != 1)
+			throw new Exception("User not found with username -" + name);
+
+		User user = userList.get(0);
+		userReturnData.setUsername(user.getUserName());
+		userReturnData.setId(user.getUserId());
+		userReturnData.setRoles(fetchRolesFromSet(user.getRoles()));
+		return userReturnData;
+	}
+
 }
