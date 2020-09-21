9301370d004dc8d63139aeb6b86e3e9a1dd62f92
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2020-09-22 01:03:06 +0530

    Update UserDetailsService.java

--- ec-crm-service/src/main/java/com/ec/crm/Service/UserDetailsService.java
+++ ec-crm-service/src/main/java/com/ec/crm/Service/UserDetailsService.java
@@ -1,7 +1,5 @@
 package com.ec.crm.Service;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
@@ -13,85 +11,86 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import com.ec.crm.Data.UserReturnData;
 
-
 @Service
-public class UserDetailsService 
+public class UserDetailsService
 {
 	@Autowired
 	WebClient.Builder webClientBuilder;
-	
+
 	@Autowired
 	HttpServletRequest request;
-	
+
 	@Value("${common.serverurl}")
 	private String reqUrl;
-	
+
 	Logger log = LoggerFactory.getLogger(UserDetailsService.class);
-    
+
 	public UserReturnData getCurrentUser()
-    {
+	{
 		log.info("Making API Call to fetch current user");
 		try
 		{
-			UserReturnData userDetails = webClientBuilder.build()
-					    	.get()
-					    	.uri(reqUrl+"user/me")
-					    	.header("Authorization", request.getHeader("Authorization"))
-					    	.retrieve()
-					    	.bodyToMono(UserReturnData.class)
-					    	.block();
+			UserReturnData userDetails = webClientBuilder.build().get().uri(reqUrl + "user/me")
+					.header("Authorization", request.getHeader("Authorization")).retrieve()
+					.bodyToMono(UserReturnData.class).block();
 			return userDetails;
-		}
-		catch(Exception e)
+		} catch (Exception e)
 		{
 			log.info("Error API Call to fetch current user " + e);
 			return null;
 		}
-    }
+	}
 
-	public UserReturnData getUserFromId(Long assigneeId) 
+	public UserReturnData getUserFromId(Long assigneeId)
 	{
 		log.info("Making API Call to fetch user from ID");
 		try
 		{
-			
-			UserReturnData userDetails = webClientBuilder.build()
-		    	.get()
-		    	.uri(reqUrl+"user/byid/"+assigneeId)
-		    	.header("Authorization", request.getHeader("Authorization"))
-		    	.retrieve()
-		    	.bodyToMono(UserReturnData.class)
-		    	.block();
+
+			UserReturnData userDetails = webClientBuilder.build().get().uri(reqUrl + "user/byid/" + assigneeId)
+					.header("Authorization", request.getHeader("Authorization")).retrieve()
+					.bodyToMono(UserReturnData.class).block();
 			return userDetails;
-		}
-		catch(Exception e)
+		} catch (Exception e)
 		{
 			log.info("Error API Call to fetch user from ID " + e);
 			return null;
 		}
 	}
-	
-	public UserReturnData[] getUserList() 
+
+	public UserReturnData getIdFromUsername(String name)
+	{
+		log.info("Making API Call to fetch userid from name");
+		try
+		{
+
+			UserReturnData userDetails = webClientBuilder.build().get().uri(reqUrl + "user/byname/" + name)
+					.header("Authorization", request.getHeader("Authorization")).retrieve()
+					.bodyToMono(UserReturnData.class).block();
+			return userDetails;
+		} catch (Exception e)
+		{
+			log.info("Error API Call to fetch user from ID " + e);
+			return null;
+		}
+	}
+
+	public UserReturnData[] getUserList()
 	{
 		log.info("Making API Call to fetch userlist");
 		try
 		{
-			
-			UserReturnData[] userDetails = webClientBuilder.build()
-		    	.get()
-		    	.uri(reqUrl+"user/list")
-		    	.header("Authorization", request.getHeader("Authorization"))
-		    	.retrieve()
-		    	.bodyToMono(UserReturnData[].class)
-		    	.block();
-		return userDetails;
-		}
-		catch(Exception e)
+
+			UserReturnData[] userDetails = webClientBuilder.build().get().uri(reqUrl + "user/list")
+					.header("Authorization", request.getHeader("Authorization")).retrieve()
+					.bodyToMono(UserReturnData[].class).block();
+			return userDetails;
+		} catch (Exception e)
 		{
 			log.info("Error API Call to fetch user list " + e);
 			return null;
 		}
-		
+
 	}
-	
+
 }
