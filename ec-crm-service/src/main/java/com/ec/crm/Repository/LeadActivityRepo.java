ee47aa396309db5a3373e37e8cac0a17dfcf709f
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2020-09-22 22:57:41 +0530

    LA Repo changes

--- ec-crm-service/src/main/java/com/ec/crm/Repository/LeadActivityRepo.java
+++ ec-crm-service/src/main/java/com/ec/crm/Repository/LeadActivityRepo.java
@@ -9,70 +9,70 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ec.crm.Data.LeadLastUpdatedDAO;
-import com.ec.crm.Data.LeadPageData;
 import com.ec.crm.Enums.ActivityTypeEnum;
 import com.ec.crm.Model.LeadActivity;
 import com.ec.crm.ReusableClasses.BaseRepository;
 
 @Repository
-public interface LeadActivityRepo extends BaseRepository<LeadActivity, Long>, JpaSpecificationExecutor<LeadActivity> 
+public interface LeadActivityRepo extends BaseRepository<LeadActivity, Long>, JpaSpecificationExecutor<LeadActivity>
 {
-	@Query(value="SELECT l from LeadActivity l where l.lead.leadId=:leadId and l.activityType=:activityType and l.isOpen=true")
-	List<LeadActivity> findByLeadActivityTypeOpen(@Param("leadId")Long leadId, @Param("activityType")ActivityTypeEnum activityType);
+	@Query(value = "SELECT l from LeadActivity l where l.lead.leadId=:leadId and l.activityType=:activityType and l.isOpen=true")
+	List<LeadActivity> findByLeadActivityTypeOpen(@Param("leadId") Long leadId,
+			@Param("activityType") ActivityTypeEnum activityType);
 
-	@Query(value="SELECT l from LeadActivity l where l.lead.leadId=:leadId and l.isOpen=true order by l.activityDateTime")
-	List<LeadActivity> fetchPendingActivitiesForLead(Long leadId);
+	@Query(value = "SELECT l from LeadActivity l where l.lead.leadId=:leadId and l.isOpen=true order by l.activityDateTime")
+	List<LeadActivity> fetchPendingActivitiesForLead(@Param("leadId") Long leadId);
 
-	@Query(value="SELECT l from LeadActivity l where l.lead.leadId=:leadId and l.isOpen=false order by l.activityDateTime desc")
-	List<LeadActivity> fetchPastActivitiesForLead(Long leadId);
+	@Query(value = "SELECT l from LeadActivity l where l.lead.leadId=:leadId and l.isOpen=false order by l.activityDateTime desc")
+	List<LeadActivity> fetchPastActivitiesForLead(@Param("leadId") Long leadId);
 
-	@Query(value="SELECT l from LeadActivity l where l.lead.leadId=:leadId and l.isOpen=true")
-	List<LeadActivity> findAllByOpenActivitiesByLeadId(Long leadId);
-	
-	@Query(value="SELECT la from LeadActivity la where la.lead.leadId=:leadId ORDER BY la.activityDateTime DESC")
-	List<LeadActivity> findAllActivitiesForLead(@Param("leadId")Long leadId);
+	@Query(value = "SELECT l from LeadActivity l where l.lead.leadId=:leadId and l.isOpen=true")
+	List<LeadActivity> findAllByOpenActivitiesByLeadId(@Param("leadId") Long leadId);
 
-	
-	@Query(value="SELECT la from LeadActivity la where la.isOpen = true and la.lead.leadId=:leadId "
-			+ "and la.activityDateTime > :todayDate "
-			+ "and la.activityDateTime < :tomorrowDate "
+	@Query(value = "SELECT la from LeadActivity la where la.lead.leadId=:leadId ORDER BY la.activityDateTime DESC")
+	List<LeadActivity> findAllActivitiesForLead(@Param("leadId") Long leadId);
+
+	@Query(value = "SELECT la from LeadActivity la where la.isOpen = true and la.lead.leadId=:leadId "
+			+ "and la.activityDateTime > :todayDate " + "and la.activityDateTime < :tomorrowDate "
 			+ "ORDER BY la.activityDateTime asc")
-	List<LeadActivity> getRecentPendingActivity(@Param("leadId")Long leadId,@Param("todayDate") Date todayDate,@Param("tomorrowDate") Date tomorrowDate);
+	List<LeadActivity> getRecentPendingActivity(@Param("leadId") Long leadId, @Param("todayDate") Date todayDate,
+			@Param("tomorrowDate") Date tomorrowDate);
 
-	@Query(value="SELECT la from LeadActivity la where la.isOpen = true and la.lead.leadId=:leadId order by la.activityDateTime desc")
-	List<LeadActivity> getRecentClosedActivity(Long leadId);
+	@Query(value = "SELECT la from LeadActivity la where la.isOpen = true and la.lead.leadId=:leadId order by la.activityDateTime desc")
+	List<LeadActivity> getRecentClosedActivity(@Param("leadId") Long leadId);
 
-	@Query(value="SELECT la from LeadActivity la where la.isOpen = false and la.lead.leadId=:leadId order by la.activityDateTime desc")
-	List<LeadActivity> getRecentActivityIrrespectiveOfStatus(Long leadId);
+	@Query(value = "SELECT la from LeadActivity la where la.isOpen = false and la.lead.leadId=:leadId order by la.activityDateTime desc")
+	List<LeadActivity> getRecentActivityIrrespectiveOfStatus(@Param("leadId") Long leadId);
 
-	@Query(value="SELECT la from LeadActivity la where la.isOpen = true and  la.activityDateTime>:todayEndDate "
+	@Query(value = "SELECT la from LeadActivity la where la.isOpen = true and  la.activityDateTime>:todayEndDate "
 			+ "and la.lead.leadId=:leadId order by la.activityDateTime asc ")
-	List<LeadActivity> getRecentUpcomingActivity(Long leadId,@Param("todayEndDate") Date todayEndDate);
+	List<LeadActivity> getRecentUpcomingActivity(@Param("leadId") Long leadId,
+			@Param("todayEndDate") Date todayEndDate);
 
-	@Query(value="SELECT la from LeadActivity la where la.isOpen = true and  la.activityDateTime<:atStartOfDay "
+	@Query(value = "SELECT la from LeadActivity la where la.isOpen = true and  la.activityDateTime<:atStartOfDay "
 			+ "and la.lead.leadId=:leadId order by la.activityDateTime desc ")
-	List<LeadActivity> getRecentPastActivity(Long leadId, Date atStartOfDay);
-	
-	@Query(value="SELECT new com.ec.crm.Data.LeadLastUpdatedDAO(l.lead.leadId,max(l.modified)) from LeadActivity l group by l.lead.leadId")
+	List<LeadActivity> getRecentPastActivity(@Param("leadId") Long leadId, @Param("atStartOfDay") Date atStartOfDay);
+
+	@Query(value = "SELECT new com.ec.crm.Data.LeadLastUpdatedDAO(l.lead.leadId,max(l.modified)) from LeadActivity l group by l.lead.leadId")
 	List<LeadLastUpdatedDAO> fetchLastUpdatedDetails();
 
-	@Query(value="SELECT MAX(modified) FROM LeadActivity la where la.lead.leadId=:leadId and la.lead.status not in ('Deal_closed','Deal_Lost')")
-	Date fetchLastModified(@Param("leadId")Long leadId);
-	
-	@Query(value="SELECT la from LeadActivity la where la.created BETWEEN :fromdate AND :todate")
-	List<LeadActivity> getActivity(Date fromdate, Date todate);
-	
-	@Query(value="SELECT la.lead.asigneeId, count(la.lead), count(la) from LeadActivity la where "
-			+"la.activityType='Deal_Close' group by la.lead.asigneeId")
-	List getConversionRatio();
-	
-	@Query(value="SELECT count(distinct la) from LeadActivity la where la.activityType='Property_Visit' and la.creatorId=:id")
-	Long getpropertyvisit(Long id);
+	@Query(value = "SELECT MAX(modified) FROM LeadActivity la where la.lead.leadId=:leadId and la.lead.status not in ('Deal_closed','Deal_Lost')")
+	Date fetchLastModified(@Param("leadId") Long leadId);
 
-	@Query(value="SELECT la from LeadActivity la where la.lead.leadId=:leadId ORDER BY la.created desc")
+	@Query(value = "SELECT la from LeadActivity la where la.created BETWEEN :fromdate AND :todate")
+	List<LeadActivity> getActivity(@Param("fromdate") Date fromdate, @Param("todate") Date todate);
+
+	@Query(value = "SELECT la.lead.asigneeId, count(la.lead), count(la) from LeadActivity la where "
+			+ "la.activityType='Deal_Close' group by la.lead.asigneeId")
+	List getConversionRatio();
+
+	@Query(value = "SELECT count(distinct la) from LeadActivity la where la.activityType='Property_Visit' and la.creatorId=:id")
+	Long getpropertyvisit(@Param("id") Long id);
+
+	@Query(value = "SELECT la from LeadActivity la where la.lead.leadId=:leadId ORDER BY la.created desc")
 	List<LeadActivity> fetchMostRecentLeadActivity(@Param("leadId") Long leadId);
 
-	@Query(value="SELECT la from LeadActivity la where la.created>created and la.activityType IN (com.ec.crm.Enums.ActivityTypeEnum.Deal_Close"
+	@Query(value = "SELECT la from LeadActivity la where la.created>created and la.activityType IN (com.ec.crm.Enums.ActivityTypeEnum.Deal_Close"
 			+ ",com.ec.crm.Enums.ActivityTypeEnum.Deal_Lost,com.ec.crm.Enums.ActivityTypeEnum.Property_Visit)")
-	List<LeadActivity> fetchActivitiesAfter(@Param("created")Date created);
+	List<LeadActivity> fetchActivitiesAfter(@Param("created") Date created);
 }
