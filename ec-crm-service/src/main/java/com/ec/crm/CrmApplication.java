9d43a9f5339da96977f03cadfbc4301faf892fb5
*sridhar* <*bsridharpatnaik@gmail.com*>
2020-10-04 14:50:06 +0530

    Owner id in leadDao

--- ec-crm-service/src/main/java/com/ec/crm/CrmApplication.java
+++ ec-crm-service/src/main/java/com/ec/crm/CrmApplication.java
@@ -21,78 +21,83 @@ public class CrmApplication extends SpringBootServletInitializer
 	@Autowired
 	LeadActivityService leadActivityService;
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(CrmApplication.class, args);
 	}
-	
-	@Bean
-	public ModelMapper leadToLeadActivityModelMapper() 
-	{
-		ModelMapper modelMapper = new ModelMapper();
-		Converter<LeadActivity,LeadPageData> leadActivity = new AbstractConverter<LeadActivity, LeadPageData>() {
-		      @Override
-		      protected LeadPageData convert(LeadActivity la) 
-		      {
-		    	  //LeadActivity leadActivity = leadActivityService.getRecentActivityByLeadId(lead.getLeadId());
-		    	  LeadPageData leadPageData = new LeadPageData();
-		    	  leadPageData.setAssigneeId(la.getLead().getAsigneeId());
-		    	  leadPageData.setLeadId(la.getLead().getLeadId());
-		    	  leadPageData.setLeadStatus(la.getLead().getStatus());
-		    	  leadPageData.setMobileNumber(la.getLead().getPrimaryMobile());
-		    	  leadPageData.setName(la.getLead().getCustomerName());
-		    	  leadPageData.setActivityDateTime(la.getActivityDateTime());
-		    	  leadPageData.setActivityType(la.getActivityType());
-		    	  leadPageData.setIsOpen(la.getIsOpen());
-		    	  return leadPageData;
-		      }
-		    };
 
-		    modelMapper.addConverter(leadActivity);
-	    return modelMapper;
-	} 
-	
 	@Bean
-	public ModelMapper leadToLeadDAOModelMapper() 
+	public ModelMapper leadToLeadActivityModelMapper()
 	{
 		ModelMapper modelMapper = new ModelMapper();
-		Converter<Lead,LeadDAO> leadActivity = new AbstractConverter<Lead, LeadDAO>() 
+		Converter<LeadActivity, LeadPageData> leadActivity = new AbstractConverter<LeadActivity, LeadPageData>()
 		{
-		      @Override
-		      protected LeadDAO convert(Lead l) 
-		      {
-		    	  //LeadActivity leadActivity = leadActivityService.getRecentActivityByLeadId(lead.getLeadId());
-		    	  LeadDAO leadDAO = new LeadDAO();
-		    	  leadDAO.setSentiment(l.getSentiment());
-		    	  leadDAO.setAddr_line1(l.getAddress().getAddr_line1());
-		    	  leadDAO.setAddr_line2(l.getAddress().getAddr_line2());
-		    	  leadDAO.setCity(l.getAddress().getCity());
-		    	  leadDAO.setPincode(l.getAddress().getPincode());
-		    	  leadDAO.setAsigneeId(l.getAsigneeId());
-		    	  leadDAO.setBroker(l.getBroker()==null?"":l.getBroker().getBrokerName());
-		    	  leadDAO.setCreatorId(l.getCreatorId());
-		    	  leadDAO.setCustomerName(l.getCustomerName());
-		    	  leadDAO.setDateOfBirth(l.getDateOfBirth());
-		    	  leadDAO.setEmailId(l.getEmailId());
-		    	  leadDAO.setLeadId(l.getLeadId());
-		    	  leadDAO.setOccupation(l.getOccupation());
-		    	  leadDAO.setPrimaryMobile(l.getPrimaryMobile());
-		    	  leadDAO.setPropertyType(l.getPropertyType());
-		    	  leadDAO.setPurpose(l.getPurpose());
-		    	  leadDAO.setSecondaryMobile(l.getSecondaryMobile());
-		    	  leadDAO.setSource(l.getSource()==null?"":l.getSource().getSourceName());
-		    	  leadDAO.setStatus(l.getStatus());
-		    	  leadDAO.setStagnantDays(leadActivityService.getStagnantDaysByLeadId(l.getLeadId()));
-		    	  return leadDAO;
-		      }
-		    };
+			@Override
+			protected LeadPageData convert(LeadActivity la)
+			{
+				// LeadActivity leadActivity =
+				// leadActivityService.getRecentActivityByLeadId(lead.getLeadId());
+				LeadPageData leadPageData = new LeadPageData();
+				leadPageData.setAssigneeId(la.getLead().getAsigneeId());
+				leadPageData.setLeadId(la.getLead().getLeadId());
+				leadPageData.setLeadStatus(la.getLead().getStatus());
+				leadPageData.setMobileNumber(la.getLead().getPrimaryMobile());
+				leadPageData.setName(la.getLead().getCustomerName());
+				leadPageData.setActivityDateTime(la.getActivityDateTime());
+				leadPageData.setActivityType(la.getActivityType());
+				leadPageData.setIsOpen(la.getIsOpen());
+				return leadPageData;
+			}
+		};
 
-		    modelMapper.addConverter(leadActivity);
-	    return modelMapper;
-	} 
-	
+		modelMapper.addConverter(leadActivity);
+		return modelMapper;
+	}
+
 	@Bean
-	public ModelMapper modelMapper() 
+	public ModelMapper leadToLeadDAOModelMapper()
 	{
-	    return new ModelMapper();
+		ModelMapper modelMapper = new ModelMapper();
+		Converter<Lead, LeadDAO> leadActivity = new AbstractConverter<Lead, LeadDAO>()
+		{
+			@Override
+			protected LeadDAO convert(Lead l)
+			{
+				// LeadActivity leadActivity =
+				// leadActivityService.getRecentActivityByLeadId(lead.getLeadId());
+				LeadDAO leadDAO = new LeadDAO();
+				leadDAO.setSentiment(l.getSentiment());
+				leadDAO.setAddr_line1(l.getAddress().getAddr_line1());
+				leadDAO.setAddr_line2(l.getAddress().getAddr_line2());
+				leadDAO.setCity(l.getAddress().getCity());
+				leadDAO.setPincode(l.getAddress().getPincode());
+				leadDAO.setAsigneeId(l.getAsigneeId());
+				leadDAO.setBroker(l.getBroker() == null ? "" : l.getBroker().getBrokerName());
+				leadDAO.setCreatorId(l.getCreatorId());
+				leadDAO.setCustomerName(l.getCustomerName());
+				leadDAO.setDateOfBirth(l.getDateOfBirth());
+				leadDAO.setEmailId(l.getEmailId());
+				leadDAO.setLeadId(l.getLeadId());
+				leadDAO.setOccupation(l.getOccupation());
+				leadDAO.setPrimaryMobile(l.getPrimaryMobile());
+				leadDAO.setPropertyType(l.getPropertyType());
+				leadDAO.setPurpose(l.getPurpose());
+				leadDAO.setSecondaryMobile(l.getSecondaryMobile());
+				leadDAO.setSource(l.getSource() == null ? "" : l.getSource().getSourceName());
+				leadDAO.setStatus(l.getStatus());
+				leadDAO.setAssigneeUserId(l.getAsigneeId());
+				leadDAO.setStagnantDays(leadActivityService.getStagnantDaysByLeadId(l.getLeadId()));
+				return leadDAO;
+			}
+		};
+
+		modelMapper.addConverter(leadActivity);
+		return modelMapper;
+	}
+
+	@Bean
+	public ModelMapper modelMapper()
+	{
+		return new ModelMapper();
 	}
 }
--- ec-crm-service/src/main/java/com/ec/crm/Data/LeadDAO.java
+++ ec-crm-service/src/main/java/com/ec/crm/Data/LeadDAO.java
@@ -2,73 +2,62 @@ package com.ec.crm.Data;
 
 import java.util.Date;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-
-import org.springframework.lang.NonNull;
 
 import com.ec.crm.Enums.LeadStatusEnum;
 import com.ec.crm.Enums.PropertyTypeEnum;
 import com.ec.crm.Enums.SentimentEnum;
-import com.ec.crm.Model.Address;
-import com.ec.crm.Model.Broker;
-import com.ec.crm.Model.Source;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import Deserializers.ToUsernameSerializer;
 import lombok.Data;
 
 @Data
-public class LeadDAO 
+public class LeadDAO
 {
 	Long leadId;
-	
+
 	String customerName;
-	
+
 	String primaryMobile;
-	
+
 	String secondaryMobile;
-	
+
 	String emailId;
-	
+
 	String purpose;
 
 	String occupation;
-	
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	Date dateOfBirth;
-	
+
 	String broker;
-	
+
 	String addr_line1;
 	String addr_line2;
 	String city;
-	String pincode; 
-	
+	String pincode;
+
 	String source;
-	
+
 	@Enumerated(EnumType.STRING)
 	PropertyTypeEnum propertyType;
-	
 
 	@Enumerated(EnumType.STRING)
 	SentimentEnum sentiment;
-	
-	@JsonSerialize(using=ToUsernameSerializer.class)
+
+	@JsonSerialize(using = ToUsernameSerializer.class)
 	Long asigneeId;
 
-	@JsonSerialize(using=ToUsernameSerializer.class)
+	Long assigneeUserId;
+
+	@JsonSerialize(using = ToUsernameSerializer.class)
 	Long creatorId;
-	
-	LeadStatusEnum status; 
-	
+
+	LeadStatusEnum status;
+
 	Long stagnantDays;
 }
