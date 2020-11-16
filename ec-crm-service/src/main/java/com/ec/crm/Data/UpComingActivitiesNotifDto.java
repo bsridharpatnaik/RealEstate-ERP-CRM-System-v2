ce2b295a5d0eefabd871932cf1802c702c274843
sree-vidya <sreevidya.h@gmail.com>
2020-11-16 22:27:00 +0530

    Unable to use the LANotificationRequestDto
    Unable to use the LANotificationRequestDto. CRM needs a dependency on common. To be discussed.

---
+++ ec-crm-service/src/main/java/com/ec/crm/Data/UpComingActivitiesNotifDto.java
@@ -0,0 +1,12 @@
+package com.ec.crm.Data;
+
+import lombok.Data;
+
+@Data
+public class UpComingActivitiesNotifDto
+{
+	private String target = "fg0vkrfbTLuYmE7va1XDHu:APA91bGQGwilPNj7UQyRg_7m9BaMtHirWXtyXI_RKfDdkPjqvqMdA7QZZEYEXlm4wbYM_FOJrSLHDTxtRrUxRlgPWm5oylBzcV-7H4M-VufYdqdLvlcMWW9SpfXMbBptCEK3cVh2gxuw";//= "clyuiIQxS2CIadcRcxrE-9:APA91bFMrqDiAbbuUU06aj2qZMMIEmbeHFiC40MKe4-LOe30HnIPqZwJXR-Gy_saCO0c5YJvGqwNBJPUCaHny4YpFEqa9uLbaMMKHx2yAIxzAQNm6HMLnssvln_Ltfe0oXQFNdxPzfsa";
+	private String title;
+	private String body;
+}
+
