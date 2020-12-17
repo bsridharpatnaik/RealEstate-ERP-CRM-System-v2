a6b8e646223c7d56ffdecf88cba5e1d93017a862
*sridhar* <*bsridharpatnaik@gmail.com*>
2020-12-18 01:38:36 +0530

    Update MachineryOnRent.java

--- sc-inventory-service/src/main/java/com/ec/application/model/MachineryOnRent.java
+++ sc-inventory-service/src/main/java/com/ec/application/model/MachineryOnRent.java
@@ -26,7 +26,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-
 @Entity
 @Table(name = "machinery_on_rent")
 @Audited
@@ -35,145 +34,205 @@ public class MachineryOnRent extends ReusableFields
 {
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	Long morid;
-	
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	@Column(nullable = false)
 	@NonNull
 	Date date;
-	
-	@ManyToOne(fetch=FetchType.LAZY,cascade = CascadeType.ALL)
-	@JoinColumn(name="machineryId",nullable=false)
-	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name = "machineryId", nullable = false)
+	@JsonIgnoreProperties(
+	{ "hibernateLazyInitializer", "handler" })
 	Machinery machinery;
-	
-	@ManyToOne(fetch=FetchType.LAZY,cascade = CascadeType.ALL)
-	@JoinColumn(name="contact_id",nullable=false)
-	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name = "contact_id", nullable = false)
+	@JsonIgnoreProperties(
+	{ "hibernateLazyInitializer", "handler" })
 	Supplier supplier;
-	
-	@ManyToOne(fetch=FetchType.LAZY,cascade = CascadeType.ALL)
-	@JoinColumn(name="locationId",nullable=false)
-	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name = "locationId", nullable = false)
+	@JsonIgnoreProperties(
+	{ "hibernateLazyInitializer", "handler" })
 	UsageLocation usageLocation;
-	
+
 	String vehicleNo;
-	
+
 	String additionalNotes;
-	
+
 	String mode;
-	
+
 	@JsonProperty("startDate")
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	Date startDate;
-	
+
 	@JsonProperty("endDate")
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	Date endDate;
-	
-	@ManyToMany(fetch=FetchType.EAGER,cascade = CascadeType.ALL)
-	@JoinTable(name = "mor_fileinformation", joinColumns = {
-			@JoinColumn(name = "morid", referencedColumnName = "morid") }, inverseJoinColumns = {
-					@JoinColumn(name = "id", referencedColumnName = "id") })
+
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(name = "mor_fileinformation", joinColumns =
+	{ @JoinColumn(name = "morid", referencedColumnName = "morid") }, inverseJoinColumns =
+	{ @JoinColumn(name = "id", referencedColumnName = "id") })
 	Set<FileInformation> fileInformations = new HashSet<>();
-	
+
 	Double initialMeterReading;
 	Double endMeterReading;
 	Double noOfTrips;
 	Double amountCharged;
-	
-	
-	
-	public Long getMorid() {
+
+	public Long getMorid()
+	{
 		return morid;
 	}
-	public void setMorid(Long morid) {
+
+	public void setMorid(Long morid)
+	{
 		this.morid = morid;
 	}
-	public Set<FileInformation> getFileInformations() {
+
+	public Set<FileInformation> getFileInformations()
+	{
 		return fileInformations;
 	}
-	public void setFileInformations(Set<FileInformation> fileInformations) {
+
+	public void setFileInformations(Set<FileInformation> fileInformations)
+	{
 		this.fileInformations = fileInformations;
 	}
-	public Date getDate() {
+
+	public Date getDate()
+	{
 		return date;
 	}
-	public void setDate(Date date) {
+
+	public void setDate(Date date)
+	{
 		this.date = date;
 	}
-	public Machinery getMachinery() {
+
+	public Machinery getMachinery()
+	{
 		return machinery;
 	}
-	public void setMachinery(Machinery machinery) {
+
+	public void setMachinery(Machinery machinery)
+	{
 		this.machinery = machinery;
 	}
-	public Supplier getSupplier() {
+
+	public Supplier getSupplier()
+	{
 		return supplier;
 	}
-	public void setSupplier(Supplier supplier) {
+
+	public void setSupplier(Supplier supplier)
+	{
 		this.supplier = supplier;
 	}
-	public UsageLocation getUsageLocation() {
+
+	public UsageLocation getUsageLocation()
+	{
 		return usageLocation;
 	}
-	public void setUsageLocation(UsageLocation usageLocation) {
+
+	public void setUsageLocation(UsageLocation usageLocation)
+	{
 		this.usageLocation = usageLocation;
 	}
-	public String getVehicleNo() {
+
+	public String getVehicleNo()
+	{
 		return vehicleNo;
 	}
-	public void setVehicleNo(String vehicleNo) {
+
+	public void setVehicleNo(String vehicleNo)
+	{
 		this.vehicleNo = vehicleNo;
 	}
-	public String getAdditionalNotes() {
+
+	public String getAdditionalNotes()
+	{
 		return additionalNotes;
 	}
-	public void setAdditionalNotes(String additionalNotes) {
+
+	public void setAdditionalNotes(String additionalNotes)
+	{
 		this.additionalNotes = additionalNotes;
 	}
-	public String getMode() {
+
+	public String getMode()
+	{
 		return mode;
 	}
-	public void setMode(String mode) {
+
+	public void setMode(String mode)
+	{
 		this.mode = mode;
 	}
-	public Date getStartDate() {
+
+	public Date getStartDate()
+	{
 		return startDate;
 	}
-	public void setStartDate(Date startDate) {
+
+	public void setStartDate(Date startDate)
+	{
 		this.startDate = startDate;
 	}
-	public Date getEndDate() {
+
+	public Date getEndDate()
+	{
 		return endDate;
 	}
-	public void setEndDate(Date endDate) {
+
+	public void setEndDate(Date endDate)
+	{
 		this.endDate = endDate;
 	}
-	public Double getInitialMeterReading() {
+
+	public Double getInitialMeterReading()
+	{
 		return initialMeterReading;
 	}
-	public void setInitialMeterReading(Double initialMeterReading) {
+
+	public void setInitialMeterReading(Double initialMeterReading)
+	{
 		this.initialMeterReading = initialMeterReading;
 	}
-	public Double getEndMeterReading() {
+
+	public Double getEndMeterReading()
+	{
 		return endMeterReading;
 	}
-	public void setEndMeterReading(Double endMeterReading) {
+
+	public void setEndMeterReading(Double endMeterReading)
+	{
 		this.endMeterReading = endMeterReading;
 	}
-	public Double getNoOfTrips() {
+
+	public Double getNoOfTrips()
+	{
 		return noOfTrips;
 	}
-	public void setNoOfTrips(Double noOfTrips) {
+
+	public void setNoOfTrips(Double noOfTrips)
+	{
 		this.noOfTrips = noOfTrips;
 	}
-	public Double getAmountCharged() {
+
+	public Double getAmountCharged()
+	{
 		return amountCharged;
 	}
-	public void setAmountCharged(Double amountCharged) {
+
+	public void setAmountCharged(Double amountCharged)
+	{
 		this.amountCharged = amountCharged;
 	}
-	
+
 }
