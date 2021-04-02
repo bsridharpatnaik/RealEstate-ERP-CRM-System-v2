e5273749d4689a16f705b1bc09d389d7acb07995
*sridhar* <bsridharpatnaik@gmail.com>
2021-04-02 18:48:33 +0530

    Merge changes

--- sc-inventory-service/src/main/java/com/ec/application/ReusableClasses/ScheduledTasks.java
+++ sc-inventory-service/src/main/java/com/ec/application/ReusableClasses/ScheduledTasks.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -22,20 +23,34 @@ public class ScheduledTasks
 {
 	@Autowired
 	StockService stockService;
-	
+
+	@Value("${schemas.list}")
+	private String schemasList;
+
 	Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
 	
 	@Scheduled(cron = "0 0 9,18 * * *")
 	public void sendStockNotificationEmailInEvening() throws Exception 
 	{
 		log.info("Sending Stock Notification Email in evening");
-		stockService.sendStockNotificationEmail();
+		String[] tenants = schemasList.split(",");
+		for(String tenantName:tenants)
+		{
+			com.ec.application.multitenant.ThreadLocalStorage.setTenantName(tenantName);
+			stockService.sendStockNotificationEmail();
+			com.ec.application.multitenant.ThreadLocalStorage.setTenantName(null);
+		}
 	}
 	
 	@Scheduled(cron = "0 0 18 * * ?")
 	public void sendStockValidationEmail() throws Exception 
 	{
 		log.info("Sending Stock Notification Email in evening");
-		stockService.sendStockValidationEmail();
+		String[] tenants = schemasList.split(",");
+		for(String tenantName:tenants) {
+			com.ec.application.multitenant.ThreadLocalStorage.setTenantName(tenantName);
+			stockService.sendStockValidationEmail();
+			com.ec.application.multitenant.ThreadLocalStorage.setTenantName(null);
+		}
 	}
 }
