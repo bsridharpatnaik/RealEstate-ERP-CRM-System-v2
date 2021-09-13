d19f2f2f140e301b52e93db61bad383bf91b7204
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2021-09-13 19:17:34 +0530

    Add comment to export

--- sc-inventory-service/src/main/java/com/ec/application/data/OutwardInventoryExportDAO2.java
+++ sc-inventory-service/src/main/java/com/ec/application/data/OutwardInventoryExportDAO2.java
@@ -11,210 +11,194 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 @JsonPropertyOrder(
-{ "Outward ID", "Outward Date", "Slip No","Category", "Inventory", "Measurement Unit", "Opening Stock", "Quantity",
-		"Closing Stock", "Warehouse", "Contractor", "Building Unit", "Final Location" })
-public class OutwardInventoryExportDAO2
-{
-	@JsonProperty("Outward ID")
-	Long outwardid;
+        {"Outward ID", "Outward Date", "Slip No", "Category", "Inventory", "Measurement Unit", "Opening Stock", "Quantity",
+                "Closing Stock", "Warehouse", "Contractor", "Building Unit", "Final Location"})
+public class OutwardInventoryExportDAO2 {
+    @JsonProperty("Outward ID")
+    Long outwardid;
 
-	@JsonProperty("Outward Date")
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
-	Date date;
+    @JsonProperty("Outward Date")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    Date date;
 
-	@JsonProperty("Purpose")
-	String purpose;
+    @JsonProperty("Purpose")
+    String purpose;
 
-	@JsonProperty("Slip No")
-	String slipNo;
+    @JsonProperty("Slip No")
+    String slipNo;
 
-	@JsonProperty("Inventory")
-	String product;
+    @JsonProperty("Inventory")
+    String product;
 
-	@JsonProperty("Measurement Unit")
-	String measurementUnit;
+    @JsonProperty("Measurement Unit")
+    String measurementUnit;
 
-	@JsonProperty("Category")
-	String category;
+    @JsonProperty("Category")
+    String category;
 
-	@JsonProperty("Opening Stock")
-	@JsonSerialize(using = DoubleTwoDigitDecimalSerializer.class)
-	Double openingStock;
+    @JsonProperty("Opening Stock")
+    @JsonSerialize(using = DoubleTwoDigitDecimalSerializer.class)
+    Double openingStock;
 
-	@JsonProperty("Quantity")
-	@JsonSerialize(using = DoubleTwoDigitDecimalSerializer.class)
-	Double quantity;
+    @JsonProperty("Quantity")
+    @JsonSerialize(using = DoubleTwoDigitDecimalSerializer.class)
+    Double quantity;
 
-	@JsonProperty("Closing Stock")
-	@JsonSerialize(using = DoubleTwoDigitDecimalSerializer.class)
-	Double closingStock;
+    @JsonProperty("Closing Stock")
+    @JsonSerialize(using = DoubleTwoDigitDecimalSerializer.class)
+    Double closingStock;
 
-	@JsonProperty("Warehouse")
-	String warehouse;
+    @JsonProperty("Warehouse")
+    String warehouse;
 
-	@JsonProperty("Contractor")
-	String contractor;
+    @JsonProperty("Contractor")
+    String contractor;
 
-	@JsonProperty("Building Unit")
-	String usageArea;
+    @JsonProperty("Building Unit")
+    String usageArea;
 
-	@JsonProperty("Final Location")
-	String usageLocation;
+    @JsonProperty("Final Location")
+    String usageLocation;
 
-	public OutwardInventoryExportDAO2(OutwardInventory oi, InwardOutwardList iol)
-	{
-		super();
-		this.outwardid = oi.getOutwardid();
-		this.date = oi.getDate();
-		this.purpose = oi.getPurpose() == null ? "" : oi.getPurpose();
-		this.slipNo = oi.getSlipNo() == null ? "" : oi.getSlipNo();
-		this.product = iol.getProduct().getProductName();
-		this.quantity = iol.getQuantity();
-		this.closingStock = iol.getClosingStock();
-		this.measurementUnit = iol.getProduct().getMeasurementUnit();
-		this.warehouse = oi.getWarehouse().getWarehouseName();
-		this.usageArea = oi.getUsageArea().getUsageAreaName();
-		this.contractor = oi.getContractor().getName();
-		this.usageLocation = oi.getUsageLocation().getLocationName();
-		this.openingStock = iol.getQuantity() + iol.getClosingStock();
-		this.category=iol.getProduct().getCategory().getCategoryName();
-	}
+    @JsonProperty("Comment")
+    String comment;
 
-	public String getCategory() {
-		return category;
-	}
+    public OutwardInventoryExportDAO2(OutwardInventory oi, InwardOutwardList iol) {
+        super();
+        this.outwardid = oi.getOutwardid();
+        this.date = oi.getDate();
+        this.purpose = oi.getPurpose() == null ? "" : oi.getPurpose();
+        this.slipNo = oi.getSlipNo() == null ? "" : oi.getSlipNo();
+        this.product = iol.getProduct().getProductName();
+        this.quantity = iol.getQuantity();
+        this.closingStock = iol.getClosingStock();
+        this.measurementUnit = iol.getProduct().getMeasurementUnit();
+        this.warehouse = oi.getWarehouse().getWarehouseName();
+        this.usageArea = oi.getUsageArea().getUsageAreaName();
+        this.contractor = oi.getContractor().getName();
+        this.usageLocation = oi.getUsageLocation().getLocationName();
+        this.openingStock = iol.getQuantity() + iol.getClosingStock();
+        this.category = iol.getProduct().getCategory().getCategoryName();
+        this.comment = oi.getAdditionalInfo();
+    }
 
-	public void setCategory(String category) {
-		this.category = category;
-	}
+    public String getCategory() {
+        return category;
+    }
 
-	public Double getOpeningStock()
-	{
-		return openingStock;
-	}
+    public void setCategory(String category) {
+        this.category = category;
+    }
 
-	public void setOpeningStock(Double openingStock)
-	{
-		this.openingStock = openingStock;
-	}
+    public Double getOpeningStock() {
+        return openingStock;
+    }
 
-	public Long getOutwardid()
-	{
-		return outwardid;
-	}
+    public void setOpeningStock(Double openingStock) {
+        this.openingStock = openingStock;
+    }
 
-	public void setOutwardid(Long outwardid)
-	{
-		this.outwardid = outwardid;
-	}
+    public Long getOutwardid() {
+        return outwardid;
+    }
 
-	public Date getDate()
-	{
-		return date;
-	}
+    public void setOutwardid(Long outwardid) {
+        this.outwardid = outwardid;
+    }
 
-	public void setDate(Date date)
-	{
-		this.date = date;
-	}
+    public Date getDate() {
+        return date;
+    }
 
-	public String getPurpose()
-	{
-		return purpose;
-	}
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
-	public void setPurpose(String purpose)
-	{
-		this.purpose = purpose;
-	}
+    public String getPurpose() {
+        return purpose;
+    }
 
-	public String getSlipNo()
-	{
-		return slipNo;
-	}
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
 
-	public void setSlipNo(String slipNo)
-	{
-		this.slipNo = slipNo;
-	}
+    public String getSlipNo() {
+        return slipNo;
+    }
 
-	public String getProduct()
-	{
-		return product;
-	}
+    public void setSlipNo(String slipNo) {
+        this.slipNo = slipNo;
+    }
 
-	public void setProduct(String product)
-	{
-		this.product = product;
-	}
+    public String getProduct() {
+        return product;
+    }
 
-	public String getMeasurementUnit()
-	{
-		return measurementUnit;
-	}
+    public void setProduct(String product) {
+        this.product = product;
+    }
 
-	public void setMeasurementUnit(String measurementUnit)
-	{
-		this.measurementUnit = measurementUnit;
-	}
+    public String getMeasurementUnit() {
+        return measurementUnit;
+    }
 
-	public Double getQuantity()
-	{
-		return quantity;
-	}
+    public void setMeasurementUnit(String measurementUnit) {
+        this.measurementUnit = measurementUnit;
+    }
 
-	public void setQuantity(Double quantity)
-	{
-		this.quantity = quantity;
-	}
+    public Double getQuantity() {
+        return quantity;
+    }
 
-	public Double getClosingStock()
-	{
-		return closingStock;
-	}
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
 
-	public void setClosingStock(Double closingStock)
-	{
-		this.closingStock = closingStock;
-	}
+    public Double getClosingStock() {
+        return closingStock;
+    }
 
-	public String getWarehouse()
-	{
-		return warehouse;
-	}
+    public void setClosingStock(Double closingStock) {
+        this.closingStock = closingStock;
+    }
 
-	public void setWarehouse(String warehouse)
-	{
-		this.warehouse = warehouse;
-	}
+    public String getWarehouse() {
+        return warehouse;
+    }
 
-	public String getUsageArea()
-	{
-		return usageArea;
-	}
+    public void setWarehouse(String warehouse) {
+        this.warehouse = warehouse;
+    }
 
-	public void setUsageArea(String usageArea)
-	{
-		this.usageArea = usageArea;
-	}
+    public String getUsageArea() {
+        return usageArea;
+    }
 
-	public String getContractor()
-	{
-		return contractor;
-	}
+    public void setUsageArea(String usageArea) {
+        this.usageArea = usageArea;
+    }
 
-	public void setContractor(String contractor)
-	{
-		this.contractor = contractor;
-	}
+    public String getContractor() {
+        return contractor;
+    }
 
-	public String getUsageLocation()
-	{
-		return usageLocation;
-	}
+    public void setContractor(String contractor) {
+        this.contractor = contractor;
+    }
 
-	public void setUsageLocation(String usageLocation)
-	{
-		this.usageLocation = usageLocation;
-	}
+    public String getUsageLocation() {
+        return usageLocation;
+    }
+
+    public void setUsageLocation(String usageLocation) {
+        this.usageLocation = usageLocation;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
