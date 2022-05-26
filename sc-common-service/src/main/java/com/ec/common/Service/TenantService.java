60d2952cc32fc743c61ce14d448a18ae87c60a57
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2022-05-26 08:05:26 +0530

    Tenant order fix

--- sc-common-service/src/main/java/com/ec/common/Service/TenantService.java
+++ sc-common-service/src/main/java/com/ec/common/Service/TenantService.java
@@ -5,10 +5,13 @@ import com.ec.common.Model.Tenant;
 import com.ec.common.Repository.TenantRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TenantService {
@@ -24,7 +27,7 @@ public class TenantService {
 
     public List<TenantInformation> fetchTenantList() {
         List<TenantInformation> tenants = new ArrayList<TenantInformation>();
-        for (Tenant t : tenantRepo.findAll()) {
+        for (Tenant t : tenantRepo.findAll(Sort.by(Sort.Direction.DESC, "name"))) {
             tenants.add(new TenantInformation(t.getTenantLongName(),t.getName(),t.getInventory(),t.getCrm()));
         }
         return tenants;
@@ -40,6 +43,7 @@ public class TenantService {
 
     public List<TenantInformation> getAllowedTenants() throws Exception {
         List<Tenant> allowedTenantKeys = userService.findTenantsForCurrentUser();
+        allowedTenantKeys = allowedTenantKeys.stream().sorted(Comparator.comparing(Tenant::getName)).collect(Collectors.toList());
         List<TenantInformation> allowedTenants = new ArrayList<TenantInformation>();
         for (Tenant ti : allowedTenantKeys) {
                 allowedTenants.add(new TenantInformation(
