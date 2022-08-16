7d5c12acf93b47d0d49c2397c9b3f0dadd336fde
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2022-08-16 13:44:27 +0530

    Fixes

--- sc-inventory-service/src/main/java/com/ec/application/service/AuthorizationService.java
+++ sc-inventory-service/src/main/java/com/ec/application/service/AuthorizationService.java
@@ -5,6 +5,7 @@ import com.ec.application.data.UserReturnData;
 import com.ec.application.data.UserTenantMapping;
 import com.ec.application.multitenant.ThreadLocalStorage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.ec.application.model.APICallTypeForAuthorization;
@@ -16,14 +17,23 @@ public class AuthorizationService {
     @Autowired
     UserDetailsService userService;
 
+    @Value("${spring.profiles.active}")
+    private String profile;
+
     public void exitIfReadOnly() throws Exception {
         UserReturnData currentUser = userService.getCurrentUser();
         String tenantName = ThreadLocalStorage.getTenantName();
-        Boolean isAllowed = false;
+        boolean isAllowed = false;
 
         for (UserTenantMapping ut : currentUser.getTenantList()) {
-            if (ut.getTenant().getName().equalsIgnoreCase(tenantName) && ut.getAuthorization().equals(AuthorizationEnum.FullAccess)) {
+
+            String tn = ut.getTenant().getName();
+            if (profile.contains("sc") && profile.contains("new")) {
+                tn = tn.replace("new", "");
+            }
+            if (tn.equalsIgnoreCase(tenantName) && ut.getAuthorization().equals(AuthorizationEnum.FullAccess)) {
                 isAllowed = true;
+                break;
             }
         }
 
