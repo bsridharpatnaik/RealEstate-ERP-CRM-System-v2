baa1bfcee9e4ccec97d300c1752a75b7625596f0
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2022-10-25 21:45:40 +0530

    Update CreateViews.sql

--- sc-inventory-service/src/main/resources/CreateViews.sql
+++ sc-inventory-service/src/main/resources/CreateViews.sql
@@ -546,3 +546,46 @@ INNER JOIN (
 ) latest ON latest.id = ai.id
 ) as t2 ON t1.month=t2.month AND t1.category_name=t2.category_name AND t1.product_name=t2.product_name AND t1.warehousename=t2.warehousename
 ORDER BY t1.month desc,product_name,warehousename;
+
+
+##### BOQ Status VIEW
+
+
+CREATE  VIEW `boq_status_view` AS
+SELECT   Row_number() OVER (ORDER BY `bu`.`id` ) AS `id`,
+         `p`.`productid`                         AS `productid`,
+         sum(`bu`.`quantity`)                    AS `boqquantity`,
+         `bu`.`buildingtypeid`                   AS `buildingtypeid`,
+         `bu`.`usagelocationid`                  AS `usagelocationid`,
+         `ul`.`location_name`                    AS `buildingunit`,
+         `bt`.`building_type`                    AS `buildingtype`,
+         `p`.`product_name`                      AS `product`,
+         `cg`.`category_name`                    AS `category`,
+         0.0                                     AS `outwardquantity`,
+         0                                       AS `status`
+FROM     ((((`BOQUpload` `bu`
+JOIN     `Usage_Location` `ul`)
+JOIN     `Product` `p`)
+JOIN     `Category` `cg`)
+JOIN     `building_type` `bt`)
+WHERE    ((
+                           `bu`.`usagelocationid` = `ul`.`locationid`)
+         AND      (
+                           `p`.`productid` = `bu`.`productid`)
+         AND      (
+                           `cg`.`categoryid` = `p`.`categoryid`)
+         AND      (
+                           `bu`.`buildingtypeid` = `bt`.`typeid`)
+         AND      (
+                           `bu`.`buildingtypeid` = `bu`.`buildingtypeid`)
+         AND      (
+                           `bu`.`usagelocationid` = `bu`.`usagelocationid`)
+         AND      (
+                           `bu`.`is_deleted` = false))
+GROUP BY `bu`.`buildingtypeid`,
+         `bu`.`usagelocationid`,
+         `bu`.`productid`,
+         `ul`.`location_name`,
+         `p`.`product_name`,
+         `cg`.`category_name`,
+         `p`.`productid`;
